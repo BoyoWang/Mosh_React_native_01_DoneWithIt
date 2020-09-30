408a9524b109a59713ab9eda4b11b68182caa598
BoyoWang <michael90110@gmail.com>
2020-09-30 22:34:20 +0900

    01-11 Logging

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("App inited.");
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
