8ecb679e84419e0a6e7383eb85d027f5e630a0da
BoyoWang <michael90110@gmail.com>
2020-09-30 23:03:04 +0900

    create an error

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   console.log("App inited.");
+
+  let x;
+  x.toString();
+
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
