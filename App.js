326e4ece645c511af005967c9f8e7d14805dcb91
BoyoWang <michael90110@gmail.com>
2020-10-01 20:40:37 +0900

    02-07-2 Alert

--- App.js
+++ App.js
@@ -9,20 +9,9 @@ export default function App() {
         color="orange"
         title="Click Me"
         onPress={() => {
-          Alert.alert("My title", "My message", [
-            {
-              text: "Yes",
-              onPress: () => {
-                console.log("Yes");
-              },
-            },
-            {
-              text: "No",
-              onPress: () => {
-                console.log("No");
-              },
-            },
-          ]);
+          Alert.prompt("My title", "My message", (text) => {
+            console.log(text);
+          });
         }}
       />
     </SafeAreaView>
