cb6d34cd8bc63184dfa7acdf1d304b2d72b37163
BoyoWang <michael90110@gmail.com>
2020-10-01 19:43:54 +0900

    02-05 Touchables

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import {
+  //
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
-      <Image source={require("./assets/icon.png")} />
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
+      <TouchableHighlight
+        onPress={() => {
+          console.log("image tapped");
         }}
-      />
+      >
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
