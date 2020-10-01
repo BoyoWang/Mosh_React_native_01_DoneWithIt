6b62c97851a68b8175778a836677a9d43aaf63eb
BoyoWang <michael90110@gmail.com>
2020-10-01 19:51:38 +0900

    cleanup the code

--- App.js
+++ App.js
@@ -1,33 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  //
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-  Image,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World!</Text>
-      <TouchableHighlight
-        onPress={() => {
-          console.log("image tapped");
-        }}
-      >
-        <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-      <StatusBar style="auto" />
+      {
+        //
+      }
     </SafeAreaView>
   );
 }
