d105afd0be86bb3d089ed5d36301570d4073ede8
BoyoWang <michael90110@gmail.com>
2020-10-01 14:51:05 +0900

    02-03-2 Text

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text pressed");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1}>
+      <Text numberOfLines={1} onPress={handlePress}>
         Hello World! This is a very very very very very very very very very very
         long text.
       </Text>
