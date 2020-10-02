4c1656dd225584fe6cf0799f96955b8449bdeb6a
BoyoWang <michael90110@gmail.com>
2020-10-02 21:00:21 +0900

    clean the file

--- App.js
+++ App.js
@@ -1,41 +1,12 @@
 import React from "react";
-import {
-  //
-  Dimensions,
-  StyleSheet,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-  View,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
-  console.log(Platform.OS, useDimensions(), useDeviceOrientation());
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: 70,
-        }}
-      ></View>
-    </SafeAreaView>
+    <View>
+      {
+        //
+      }
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
