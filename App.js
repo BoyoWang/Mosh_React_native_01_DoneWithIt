f8b3957b778ab64429e86be9c632394dfac2ec27
BoyoWang <michael90110@gmail.com>
2020-10-14 18:03:28 +0900

    3-11-1 Building the Welcome Screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./app/screen/WelcomeScreen";
 
 export default function App() {
-  return <View />;
+  return <WelcomeScreen />;
 }
