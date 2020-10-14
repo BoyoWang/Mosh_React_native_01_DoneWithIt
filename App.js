456be78436601983c141313fdbeebea8fc3535ad
BoyoWang <michael90110@gmail.com>
2020-10-14 17:42:38 +0900

    clear App.js file

--- App.js
+++ App.js
@@ -2,40 +2,5 @@ import React from "react";
 import { View } from "react-native";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 10,
-          left: 10,
-          position: "absolute",
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <View />;
 }
