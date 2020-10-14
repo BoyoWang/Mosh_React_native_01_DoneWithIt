88c9e18017fef970d4be155eb4c44e0d5f6cf93e
BoyoWang <michael90110@gmail.com>
2020-10-14 18:10:35 +0900

    03-11-2 Building the Welcome Screen

--- app/screen/WelcomeScreen.js
+++ app/screen/WelcomeScreen.js
@@ -6,13 +6,27 @@ function WelcomeScreen(props) {
     <ImageBackground
       style={styles.background}
       source={require("../assets/background.jpg")}
-    ></ImageBackground>
+    >
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "flex-end",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
   },
 });
 
