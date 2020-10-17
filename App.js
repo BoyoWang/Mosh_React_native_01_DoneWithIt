f59e5be807aa908a244a645e88c60186ec3dd71e
BoyoWang <michael90110@gmail.com>
2020-10-17 11:29:08 +0900

    setup before section

--- App.js
+++ App.js
@@ -4,5 +4,21 @@ import WelcomeScreen from "./app/screen/WelcomeScreen";
 import ViewImageScreen from "./app/screen/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          height: 100,
+          width: 100,
+          backgroundColor: "dodgerblue",
+        }}
+      />
+    </View>
+  );
 }
