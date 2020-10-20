4ea2fb31bf4c92fb0c2220462faf8d69db947b36
BoyoWang <michael90110@gmail.com>
2020-10-20 12:38:03 +0900

    04-09-1 Organizing Styles

--- app/components/AppText/AppText.js
+++ app/components/AppText/AppText.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { Text, StyleSheet, Platform } from "react-native";
+import styles from "./style";
 
 function AppText(props) {
   return <Text style={styles.text}>{props.children}</Text>;
 }
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-  },
-});
 
 export default AppText;
