13c86453e7957b88bd4d572d52def791019b6ebb
BoyoWang <michael90110@gmail.com>
2020-10-22 15:07:00 +0900

    setup before lesson

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-import WelcomeScreen from "./app/screen/WelcomeScreen";
-import ViewImageScreen from "./app/screen/ViewImageScreen";
-import AppText from "./app/components/AppText/";
-import AppButton from "./app/components/AppButton";
+import { View } from "react-native";
 
 export default function App() {
   return <WelcomeScreen />;
