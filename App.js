48faccf407027a3780b9430f2b8aafbb0ed452fd
BoyoWang <michael90110@gmail.com>
2020-10-31 11:56:33 +0900

    clean up app.js

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import React from "react";
 
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screen/AccountScreen";
-import ListingsScreen from "./app/screen/ListingsScreen";
 
 export default function App() {
-  return (
-    <Screen>
-      <ListingsScreen />
-    </Screen>
-  );
+  return <Screen />;
 }
