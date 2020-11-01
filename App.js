c87cae1fcb2e465484644621dec6874b7fa478db
BoyoWang <michael90110@gmail.com>
2020-11-01 15:32:43 +0900

    setup before lesson the code in AppPicker.js is borrowed from AppTextInput.js

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
-import { Switch } from "react-native";
 
-import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 
 export default function App() {
-  const [isNew, setIsNew] = useState(false);
-
-  return (
-    <Screen>
-      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
-    </Screen>
-  );
+  return <Screen />;
 }
