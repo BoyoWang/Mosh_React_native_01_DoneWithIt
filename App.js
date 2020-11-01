2b4efb488f0c7a40844347ec53f8b873dcb5fdef
BoyoWang <michael90110@gmail.com>
2020-11-01 15:53:08 +0900

    06-07 Building a Custom Picker

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
+import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
-  return <Screen />;
+  return (
+    <Screen>
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
