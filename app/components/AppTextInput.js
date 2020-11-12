bef8d12d4e1f73b4c373bb0a5c24907490dbeb4d
BoyoWang <michael90110@gmail.com>
2020-11-12 18:50:27 +0900

    07-13 Fixing the Placeholder Color

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -15,7 +15,11 @@ function AppTextInput({ icon, ...otherProps }) {
           style={styles.icon}
         />
       )}
-      <TextInput style={[defaultStyles.text, { flex: 1 }]} {...otherProps} />
+      <TextInput
+        placeholderTextColor={defaultStyles.colors.medium}
+        style={[defaultStyles.text, { flex: 1 }]}
+        {...otherProps}
+      />
     </View>
   );
 }
