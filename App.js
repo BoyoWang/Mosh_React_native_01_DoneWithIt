38767baf2010b1f0110c8717197c74050857742e
BoyoWang <michael90110@gmail.com>
2020-11-14 10:54:42 +0900

    08-02 Adding Chevrons

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import ListingEditScreen from "./app/screen/ListingEditScreen";
+import MessagesScreen from "./app/screen/MessagesScreen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  return <MessagesScreen />;
 }
