c5ff57b3330c3e2ef77106db52972dabf92bd26e
BoyoWang <michael90110@gmail.com>
2020-11-14 11:41:21 +0900

    setup before lesson

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 import ListingEditScreen from "./app/screen/ListingEditScreen";
-import MessagesScreen from "./app/screen/MessagesScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingEditScreen />;
 }
