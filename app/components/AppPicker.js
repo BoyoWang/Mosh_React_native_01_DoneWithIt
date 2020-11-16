82bda3b5625404508b12a901aed40e109ff6c240
BoyoWang <michael90110@gmail.com>
2020-11-16 10:25:54 +0900

    08-08-1 Building the CategoryPickerItem Component

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -17,6 +17,7 @@ import PickerItem from "./PickerItem";
 function AppPicker({
   icon,
   items,
+  numberOfColumns = 1,
   onSelectItem,
   PickerItemComponent = PickerItem,
   placeholder,
@@ -55,9 +56,10 @@ function AppPicker({
           <FlatList
             data={items}
             keyExtractor={(item) => item.value.toString()}
+            numColumns={numberOfColumns}
             renderItem={({ item }) => (
               <PickerItemComponent
-                label={item.label}
+                item={item}
                 onPress={() => {
                   setModalVisible(false);
                   onSelectItem(item);
--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { TouchableOpacity, View, StyleSheet } from "react-native";
+import AppText from "./AppText";
+import Icon from "./Icon";
 
-function CategoryPickerItem(props) {
-  return null;
+function CategoryPickerItem({ item, onPress }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+        <AppText style={styles.label}>{item.label}</AppText>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
+    marginTop: 5,
+    textAlign: "center",
+  },
+});
 
 export default CategoryPickerItem;
