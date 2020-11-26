87465078ed56d5d604c012b15cb27010bceb4275
BoyoWang <michael90110@gmail.com>
2020-11-26 14:51:22 +0900

    02-05 Permissions Module

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
 
 export default function App() {
   const requestPermission = async () => {
+    const result = await Permissions.askAsync(Permissions.CAMERA_ROLL);
     const { granted } = await ImagePicker.requestCameraPermissionsAsync();
     if (!granted) alert("You need to enable permission to access the library.");
   };
