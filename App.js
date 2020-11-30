611a14a435b7dee56833645e158368e5c7050715
BoyoWang <michael90110@gmail.com>
2020-11-30 17:55:13 +0800

    02-09-2 Building ImageInput- Touches

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import Screen from "./app/components/Screen";
 import ImageInput from "./app/components/ImageInput";
@@ -6,18 +6,14 @@ import ImageInput from "./app/components/ImageInput";
 export default function App() {
   const [imageUri, setImageUri] = useState();
 
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
-    } catch (error) {
-      console.log("Error reading an image", error);
-    }
-  };
-
   return (
     <Screen>
-      <ImageInput imageUri={imageUri} />
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => {
+          setImageUri(uri);
+        }}
+      />
     </Screen>
   );
 }
