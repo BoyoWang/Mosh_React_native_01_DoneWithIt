313e687858f8f161a8c46131dd1ec3b495ce0a24
BoyoWang <michael90110@gmail.com>
2020-12-01 08:35:12 +0800

    02-11-1 Building ImageInputList- Scrolling

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,18 +1,20 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import React from "react";
 
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View style={styles.image} key={uri}>
-          <ImageInput imageUri={uri} onChangeImage={onRemoveImage} />
-        </View>
-      ))}
-      <ImageInput onChangeImage={onAddImage} />
-    </View>
+    <ScrollView horizontal>
+      <View style={styles.container}>
+        {imageUris.map((uri) => (
+          <View style={styles.image} key={uri}>
+            <ImageInput imageUri={uri} onChangeImage={onRemoveImage} />
+          </View>
+        ))}
+        <ImageInput onChangeImage={onAddImage} />
+      </View>
+    </ScrollView>
   );
 }
 
