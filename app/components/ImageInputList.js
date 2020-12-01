f59cabda31318123ed4ab7be720f7b955122c897
BoyoWang <michael90110@gmail.com>
2020-12-01 09:12:13 +0800

    02-11-3 Building ImageInputList- Scrolling

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -7,20 +7,22 @@ function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
   const scrollView = useRef();
 
   return (
-    <ScrollView
-      ref={scrollView}
-      horizontal
-      onContentSizeChange={() => scrollView.current.scrollToEnd()}
-    >
-      <View style={styles.container}>
-        {imageUris.map((uri) => (
-          <View style={styles.image} key={uri}>
-            <ImageInput imageUri={uri} onChangeImage={onRemoveImage} />
-          </View>
-        ))}
-        <ImageInput onChangeImage={onAddImage} />
-      </View>
-    </ScrollView>
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View style={styles.image} key={uri}>
+              <ImageInput imageUri={uri} onChangeImage={onRemoveImage} />
+            </View>
+          ))}
+          <ImageInput onChangeImage={onAddImage} />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
