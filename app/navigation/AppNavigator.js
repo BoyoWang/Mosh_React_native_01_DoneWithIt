e43cfba02a402f250f024246892fb067a65fe415
BoyoWang <michael90110@gmail.com>
2020-12-05 14:04:00 +0800

    03-17-1 Beautifying the Tabs

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
 
 import AccountNavigator from "./AccountNavigator";
@@ -9,8 +10,44 @@ const Tab = createBottomTabNavigator();
 
 export default AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons //
+            name="home"
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="ListingEdit"
+      component={ListingEditScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons //
+            name="plus-circle"
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Account"
+      component={AccountNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons //
+            name="account"
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
