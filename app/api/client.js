f4181586da1f1dfa9c9fa2d3734289d17bf41022
BoyoWang <michael90110@gmail.com>
2020-12-07 14:30:44 +0800

    04-04 Creating an API Layer

--- app/api/client.js
+++ app/api/client.js
@@ -2,8 +2,4 @@ import { create } from "apisauce";
 
 const apiClient = create({ baseURL: "http://192.168.1.107:9000/api" });
 
-apiClient.get("/listings").then((response) => {
-  if (!response.ok) {
-    console.log(response.problem);
-  }
-});
+export default apiClient;
