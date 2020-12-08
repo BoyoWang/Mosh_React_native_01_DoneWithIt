9e8c4b0eebae0e4085a12703988038577035fb47
BoyoWang <michael90110@gmail.com>
2020-12-08 14:15:39 +0800

    04-07 Handling Errors

--- app/screen/ListingsScreen.js
+++ app/screen/ListingsScreen.js
@@ -6,9 +6,12 @@ import colors from "../config/colors";
 import listingsApi from "../api/listings";
 import routes from "../navigation/routes";
 import Screen from "../components/Screen";
+import AppText from "../components/Text";
+import Button from "../components/Button";
 
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
@@ -16,12 +19,21 @@ function ListingsScreen({ navigation }) {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen style={styles.screen}>
       <View style={styles.container}>
+        {error && (
+          <>
+            <AppText>Couldn't retrieve the listings.</AppText>
+            <Button title="Retry" onPress={loadListings} />
+          </>
+        )}
         <FlatList
           data={listings}
           keyExtractor={(listing) => listing.id.toString()}
