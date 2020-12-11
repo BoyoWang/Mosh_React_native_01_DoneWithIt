c0d47bfdb42ba050268b319845eeb9907d700d85
BoyoWang <michael90110@gmail.com>
2020-12-11 14:22:22 +0800

    05-03-2 Detecting Network Status NetInfo addEventListener method

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import React from "react";
 import NetInfo from "@react-native-community/netinfo";
 
 export default function App() {
-  NetInfo.fetch().then((netInfo) => console.log(netInfo));
+  // componentDidMount
+  const unsubscribe = NetInfo.addEventListener((netInfo) =>
+    console.log(netInfo)
+  );
+
+  // componentWillMount
+  unsubscribe();
+
   return null;
 }
