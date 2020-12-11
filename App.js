24e4c8556c13038b83dffe3fff277fb1db9a2234
BoyoWang <michael90110@gmail.com>
2020-12-11 14:52:07 +0800

    clear App.js

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
-import { Button, View } from "react-native";
 
 export default function App() {
-  const netInfo = useNetInfo();
-
-  return <Button disabled={!netInfo.isInternetReachable} title="Btn" />;
+  return null;
 }
