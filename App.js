f4bc8fae3c8f0461a1e84c7e6dd3562a4b06579a
BoyoWang <michael90110@gmail.com>
2020-12-11 15:08:18 +0800

    05-05 AsyncStorage

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
 
 export default function App() {
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  demo();
+
   return null;
 }
