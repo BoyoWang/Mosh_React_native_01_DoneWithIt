f72ad7c38743063644ff45869dffe5c4de788197
BoyoWang <michael90110@gmail.com>
2020-12-11 14:20:06 +0800

    05-03-1 Detecting Network Status NetInfo fetch method

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import NetInfo from "@react-native-community/netinfo";
 
 export default function App() {
+  NetInfo.fetch().then((netInfo) => console.log(netInfo));
   return null;
 }
