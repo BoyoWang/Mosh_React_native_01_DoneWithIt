88835edf888bcf23dd6f8f31fc8cc56c37f6490b
BoyoWang <michael90110@gmail.com>
2020-12-11 21:59:28 +0800

    05-08 Implementing a Cache Layer

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import React from "react";
+import cache from "./app/utility/cache";
 
 export default function App() {
   const demo = async () => {
-    try {
-      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
-      const value = await AsyncStorage.getItem("person");
-      const person = JSON.parse(value);
-      console.log(person);
-    } catch (error) {
-      console.log(error);
-    }
+    await cache.store("person", { id: 1 });
+    const person = await cache.get("person");
+    console.log(person);
   };
 
   demo();
