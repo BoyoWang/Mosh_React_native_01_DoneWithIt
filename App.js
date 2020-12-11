8962f156729eda56514c4a208949ba56a0806654
BoyoWang <michael90110@gmail.com>
2020-12-11 14:13:11 +0800

    Clean up App.js

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 
-import AppNavigator from "./app/navigation/AppNavigator";
-import AuthNavigator from "./app/navigation/AuthNavigator";
-import navigationTheme from "./app/navigation/navigationTheme";
-
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  return null;
 }
