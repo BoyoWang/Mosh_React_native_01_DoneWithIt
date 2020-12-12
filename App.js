69e123b896cf69d456b074f43a8b3aadee36365c
BoyoWang <michael90110@gmail.com>
2020-12-12 19:30:08 +0800

    05-14-1 Showing an Offline Notice Creating the OfflineNotice component

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import React from "react";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
