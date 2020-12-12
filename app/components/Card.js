196c8d8159be8c1f0b263b4a1aad8f959f8c7d32
BoyoWang <michael90110@gmail.com>
2020-12-12 15:33:26 +0800

    05-10-1 Caching Images implementation

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,10 +1,7 @@
 import React from "react";
-import {
-  View,
-  Image,
-  StyleSheet,
-  TouchableWithoutFeedback,
-} from "react-native";
+import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+
+import { Image } from "react-native-expo-image-cache";
 
 import colors from "../config/colors";
 import Text from "./Text";
@@ -13,7 +10,7 @@ function Card({ title, subTitle, imageUrl, onPress }) {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={{ uri: imageUrl }} />
+        <Image style={styles.image} uri={imageUrl} />
         <View style={styles.detailsContainer}>
           <Text style={styles.title}>{title}</Text>
           <Text style={styles.subTitle}>{subTitle}</Text>
