f2676f03c81d74f7d7cba3c80adb857344d5e076
BoyoWang <michael90110@gmail.com>
2020-12-17 13:31:15 +0800

    06-05 Getting the AuthToken

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import React from "react";
 import AppNavigator from "./app/navigation/AppNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import OfflineNotice from "./app/components/OfflineNotice";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </>
   );
