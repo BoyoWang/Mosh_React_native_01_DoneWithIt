f1ec3a7acd60cb575878164be8eba47c209792ed
BoyoWang <michael90110@gmail.com>
2021-01-21 22:23:38 +0900

    06-17-2 Showing an Activity Indicator

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -10,12 +10,7 @@ export default useApi = (apiFunc) => {
     const response = await apiFunc(...args);
     setLoading(false);
 
-    if (!response.ok) {
-      setError(true);
-      return response;
-    }
-
-    setError(false);
+    setError(!response.ok);
     setData(response.data);
     return response;
   };
