64bf9a4a75955a6fc32662fefb7a9f416fb60469
BoyoWang <michael90110@gmail.com>
2021-01-23 21:41:59 +0900

    07-11 Local Notifications

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
-import { NavigationContainer } from "@react-navigation/native";
-import React, { useState } from "react";
-import { AppLoading } from "expo";
+import React from "react";
+import { Button } from "react-native";
+import * as Notifications from "expo-notifications";
 
-import AppNavigator from "./app/navigation/AppNavigator";
-import AuthContext from "./app/auth/context";
-import AuthNavigator from "./app/navigation/AuthNavigator";
-import authStorage from "./app/auth/storage";
-import navigationTheme from "./app/navigation/navigationTheme";
-import OfflineNotice from "./app/components/OfflineNotice";
-import { navigationRef } from "./app/navigation/rootNavigation";
+import Screen from "./app/components/Screen";
 
-export default function App() {
-  const [user, setUser] = useState();
-  const [isReady, setIsReady] = useState(false);
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    };
+  },
+});
 
-  const restoreUser = async () => {
-    const user = await authStorage.getUser();
-    if (user) setUser(user);
+function App(props) {
+  const showNotification = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Test title for local",
+        body: "Test body for local",
+      },
+      trigger: null,
+    });
   };
 
-  if (!isReady)
-    return (
-      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
-    );
-
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      <OfflineNotice />
-      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
-        {user ? <AppNavigator /> : <AuthNavigator />}
-      </NavigationContainer>
-    </AuthContext.Provider>
+    <Screen>
+      <Button title="Tap me" onPress={showNotification} />
+    </Screen>
   );
 }
+
+export default App;
