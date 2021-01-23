37c3504073e80de572907f5575dd79a605b19cb6
BoyoWang <michael90110@gmail.com>
2021-01-23 21:45:17 +0900

    restore the file

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
-import React from "react";
-import { Button } from "react-native";
-import * as Notifications from "expo-notifications";
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useState } from "react";
+import { AppLoading } from "expo";
 
-import Screen from "./app/components/Screen";
+import AppNavigator from "./app/navigation/AppNavigator";
+import AuthContext from "./app/auth/context";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import authStorage from "./app/auth/storage";
+import navigationTheme from "./app/navigation/navigationTheme";
+import OfflineNotice from "./app/components/OfflineNotice";
+import { navigationRef } from "./app/navigation/rootNavigation";
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => {
-    return {
-      shouldShowAlert: true,
-      shouldPlaySound: true,
-      shouldSetBadge: true,
-    };
-  },
-});
+export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
-function App(props) {
-  const showNotification = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: "Test title for local",
-        body: "Test body for local",
-      },
-      trigger: null,
-    });
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   };
 
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
+    );
+
   return (
-    <Screen>
-      <Button title="Tap me" onPress={showNotification} />
-    </Screen>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <OfflineNotice />
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
-
-export default App;
