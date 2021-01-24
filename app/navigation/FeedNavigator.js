86588ed6a4f647ba663cc304abbeec992203a293
BoyoWang <michael90110@gmail.com>
2021-01-24 17:59:12 +0900

    Feed navigator screens headers modified

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -8,14 +8,16 @@ import routes from "./routes";
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => (
-  <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
+  <Stack.Navigator mode="card">
     <Stack.Screen //
       name={routes.LISTINGS}
       component={ListingsScreen}
+      options={{ headerShown: false }}
     />
     <Stack.Screen
       name={routes.LINSTING_DETAILS}
       component={ListingDetailsScreen}
+      options={{ headerBackTitle: "Feed", title: "" }}
     />
   </Stack.Navigator>
 );
